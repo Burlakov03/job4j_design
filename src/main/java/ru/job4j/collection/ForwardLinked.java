6e24b526e7220313d7badb827ed548db00455473
Burlakov03 <bf.karatel@gmail.com>
2025-04-07 17:02:57 +0800

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,90 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private int size;
+    private int modCount;
+    private Node<T> head;
+
+    public void add(T value) {
+        if (head == null) {
+            head = new Node<>(value, null);
+        } else {
+            Node<T> s = head;
+            while (s.next != null) {
+                s = s.next;
+            }
+            s.next = new Node<>(value, null);
+        }
+        modCount++;
+        size++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> result = head;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.item;
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        Node<T> result = head;
+        head = head.next;
+        result.next = null;
+        return result.item;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LinkedIterator();
+    }
+
+    private static class Node<T> {
+        private final T item;
+        private Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+
+    private class LinkedIterator implements Iterator<T> {
+
+        private Node<T> cursor = head;
+
+        private final int expectedModCount = modCount;
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T value = cursor.item;
+            cursor = cursor.next;
+            return value;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            return cursor != null;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
